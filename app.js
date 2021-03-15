eb915360cebae1782bfd7fb261019e214433c1a6
Jalil Kettani <3053619-jkgitlabwork@users.noreply.gitlab.com>
2021-03-15 22:13:10 +0100

    Add the register controller and route + use built in middlewares to parse body

--- app.js
+++ app.js
@@ -3,13 +3,30 @@ const router = require("./router");
 
 const app = express();
 
-// Indicate express to use static files in the public folder
+/**
+ * Use built-in middleware to parse incoming requests with urlencoded payloads.
+ * A new body object containing the parsed data is populated on the request
+ * object after the middleware.
+ */
+app.use(express.urlencoded({ extended: false }));
+
+/**
+ * Use built-in middleware to parse incoming requests with JSON payloads.
+ * A new body object containing the parsed data is populated on the request
+ * object after the middleware.
+ */
+app.use(express.json());
+
+// Indicate express to use static files in the public folder.
 app.use(express.static("public"));
-// Indicate express where to find the HTML templates
+
+// Indicate express where to find the HTML templates.
 app.set("views", "views");
-// Indicate express where which template engine to use
+
+// Indicate express where which template engine to use.
 app.set("view engine", "ejs");
-// App will handle requests using the router defined in router.js
+
+// App will handle requests using the router defined in router.js.
 app.use("/", router);
 
 app.listen(3000, () => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,7 @@
 exports.home = function (req, res) {
   res.render("home-guest");
 };
+
+exports.register = function (req, res) {
+  res.send("Thanks for trying to register");
+};
